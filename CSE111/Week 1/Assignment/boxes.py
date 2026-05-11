7093d4c01f5e318f9040ad673525ff561f12563a
Simire Simeon <simiresimeon@gmail.com>
2026-05-11 19:23:27 +0100

    Refactor boxes.py to include item input and box calculation logic

--- CSE111/Week 1/Assignment/boxes.py
+++ CSE111/Week 1/Assignment/boxes.py
@@ -1,1 +1,11 @@
-number_items =
+#modules:
+import math
+
+number_items = int(input("Enter the number of items: "))
+items_perbox = int(input("Enter the number of items per box: "))
+
+number_boxes = math.ceil(number_items/items_perbox)
+
+print()
+
+print(f"For {number_items}, packing {items_perbox}, you will need {number_boxes}.")
